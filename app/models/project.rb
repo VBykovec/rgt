a38ce5ad3fd2fc6091cf5210aeb0bd581419bf50
Vitaliy Bykovec <vbykovec@gmail.com>
2013-05-05 21:56:27 +0300

    add associations for users,projects,tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
   attr_accessible :name, :user_id
+  has_many :tasks, :order => :priority
+  belongs_to :user
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   attr_accessible :deadline, :description, :priority, :project_id, :status
+  belongs_to :project
+  acts_as_list :scope => :project
 end
