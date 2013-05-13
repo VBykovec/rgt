56a874cc2df82ba26a026c4cdf7760372ee1b518
Vitaliy Bykovec <vbykovec@gmail.com>
2013-05-13 06:21:23 +0300

    fix access rights for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
   respond_to :json
   inherit_resources
+  load_and_authorize_resource
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.persisted?
       can :manage, Project, :user_id => user.id 
+      can :manage, Task, :project => {:user_id => user.id}
     end
    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
